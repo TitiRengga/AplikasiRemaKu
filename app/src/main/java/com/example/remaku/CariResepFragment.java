6997ed4c8c20b567f5f687e1716153b38ad2953c
Titi Rengga Sari <titirengga@mhs.unsyiah.ac.id>
2021-06-18 01:36:15 +0700

    edit

--- app/src/main/java/com/example/remaku/CariResepFragment.java
+++ app/src/main/java/com/example/remaku/CariResepFragment.java
@@ -20,21 +20,21 @@ public class CariResepFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        inputData("Ayam Bakar Bumbu Bali", "artikel_1.html");
-        inputData("Sate Ayam Srepeh", "artikel_2.html");
-        inputData("Pizza Sosis Jumbo (Tanpa Ulen)", "artikel_3.html");
-        inputData("Nasgor Mawut (Mawut Sayur)", "artikel_4.html");
-        inputData("Fuyung Hai", "artikel_5.html");
+        inputData("Ayam Bakar Madu", "artikel_1.html");
+        inputData("Bakmi Ayam Jamur", "artikel_2.html");
+        inputData("Bubble Tea", "artikel_3.html");
+        inputData("Coklat Truffle", "artikel_4.html");
+        inputData("Eggless Chocolate Mug Cake", "artikel_5.html");
         inputData("Lobster Bumbu Padang", "artikel_6.html");
-        inputData("Sop Iga Sapi", "artikel_7.html");
-        inputData("Opor Ayam Kampung", "artikel_8.html");
-        inputData("Bebek Goreng Sambel Ijo", "artikel_9.html");
-        inputData("Soto Ayam Kampung", "artikel_10.html");
-        inputData("Bakso Ayam", "artikel_11.html");
-        inputData("Ikan Gurame Bakar", "artikel_12.html");
+        inputData("Spaghetti Aglio Olio", "artikel_7.html");
+        inputData("Ramen Kuah Pedas", "artikel_8.html");
+        inputData("Rendang Daging", "artikel_9.html");
+        inputData("Sop Buah", "artikel_10.html");
+        inputData("Tteokbokki", "artikel_11.html");
+        inputData("Puding Brownis Coklat", "artikel_12.html");
         inputData("Pisang Bakar Coklat Keju", "artikel_13.html");
         inputData("Keto Martabak Terang Bulan", "artikel_14.html");
-        inputData("Ingkung Ayam Kampung", "artikel_15.html");
+        inputData("Tongseng Kambing Istimewa", "artikel_15.html");
         //menampilkan data ke dalam recyclerView
         View view = inflater.inflate(R.layout.activity_cariresep, container, false);
         recyclerView = view.findViewById(R.id.recyclerView);
